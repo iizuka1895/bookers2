34e703d769d2017a3561d77016895285269450c2
YuIizuka <148466726+iizuka1895@users.noreply.github.com>
2023-12-15 13:50:05 +0000

    update validtes file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,20 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
+    @book = Book.save(books_params)
   end
   
   def create
-     @books = Book.new(books_params)
-     @books.user_id = current_user.id
-     @books.save
-     redirect_to user_path(current_user)
+    @books = Book.new(books_params)
+    @books.user_id = current_user.id
+    @user = current_user
+    if @books.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to user_path(current_user)
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
   def show
@@ -29,6 +36,7 @@ class BooksController < ApplicationController
   def update
     @books = Book.find(params[:id])
     @books.update(books_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@books.id)
   end
   
