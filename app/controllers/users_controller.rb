c7d951c8797789c25d48e8e19feab1726c182b7f
YuIizuka <148466726+iizuka1895@users.noreply.github.com>
2023-12-18 12:34:12 +0000

    update view file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,12 +31,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def is_matching_login_user
-    @user = User.find(current_user.id)
-    unless @user.id == params[:id].to_i
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
